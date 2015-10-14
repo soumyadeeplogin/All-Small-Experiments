b770b0f64789c5bf2e88b0f348409fd0b2a70183
soumyadeeplogin <soumyadeeplogin@gmail.com>
2015-10-14 11:21:41 +0530

    ScreenShot Added

--- CompareImage/src/Compare.java
+++ CompareImage/src/Compare.java
@@ -1,6 +1,11 @@
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
@@ -8,11 +13,44 @@ public class Compare {
 	
 	public static void main(String args[])
 	{
-		File img1 = new File("C:\\test1.jpg");
-		File img2 = new File("C:\\test3.jpg");
+//		File img1 = new File("C:\\test1.jpg");
+//		File img2 = new File("C:\\test3.jpg");
+		
+//		System.out.println(compareImage(img1, img2));
+		screenShot("C:\\ScSh1");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		screenShot("C:\\ScSh2");
+		
+		
+		File img1 = new File("C:\\ScSh2.jpg");
+		File img2 = new File("C:\\ScSh1.jpg");
 		
 		System.out.println(compareImage(img1, img2));
-		
+	}
+	
+	public static void screenShot(String fileName)
+	{
+		try {
+            Robot robot;
+			robot = new Robot();
+            String format = "jpg";
+            fileName = fileName+"."+format;
+//            String fileName = "C:\\FullScreenshot." + format;
+             
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+//            System.out.println("A full screenshot saved!");
+       } 
+		catch(IOException | AWTException e){
+			System.err.println(e);
+		}
 	}
 	
 	public static boolean compareImage(File fileA, File fileB) {        
